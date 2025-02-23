cdd48731da0a69c29a2f82099266ebc1f584831d
leonardo-policarpo <leo.dumont@outlook.com>
2025-02-23 12:34:40 -0300

    chore: add referrer id when user subscribe to event

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,33 +1,39 @@
-import { z } from 'zod';
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+import { z } from 'zod';
 import { subscribeToEvent } from '../functions/subscribe-to-event';
 
-export const subscribeToEventRoute: FastifyPluginAsyncZod = async (app) => {
-  app.post('/subscriptions', {
-    schema: {
-      summary: 'Subscribes someone to the event',
-      tags: ['subscription'],
-      description: 'sadsdjs',
-      body: z.object({
-        name: z.string(),
-        email: z.string().email()
-      }),
-      response: {
-        201: z.object({
-          subscriberId: z.string(),
+export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
+  app.post(
+    '/subscriptions',
+    {
+      schema: {
+        summary: 'Subscribes someone to the event',
+        tags: ['subscription'],
+        description: 'sadsdjs',
+        body: z.object({
+          name: z.string(),
+          email: z.string().email(),
+          referrer: z.string().nullish(),
         }),
-      }
-    }
-  }, async (request, reply) => {
-    const { name, email } = request.body;
+        response: {
+          201: z.object({
+            subscriberId: z.string(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { name, email, referrer } = request.body;
 
-    const { subscriberId } = await subscribeToEvent({
-      name,
-      email
-    })
-  
-    return reply.status(201).send({
-      subscriberId
-    })
-  });
-}
+      const { subscriberId } = await subscribeToEvent({
+        name,
+        email,
+        referrerId: referrer,
+      });
+
+      return reply.status(201).send({
+        subscriberId,
+      });
+    }
+  );
+};
